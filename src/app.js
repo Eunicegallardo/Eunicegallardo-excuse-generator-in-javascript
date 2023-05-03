b1f495cc7005efc5df684da7652c28d2ba0e30cc
Eunice Gallardo <128846071+Eunicegallardo@users.noreply.github.com>
2023-05-03 22:11:42 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -2,10 +2,33 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+window.onload = () => {
+  document.querySelector("#the-button").addEventListener("click", () => {
+    document.querySelector("#my-excuse").innerHTML = generateExcuse();
+  });
+  console.log("Hello world!");
+};
+
+let generateExcuse = () => {
+  let who = ["The dog", "My sister", "His turtle", "My bird"];
+  let action = ["ate", "ran on", "crushed", "broke"];
+  let what = ["my homework", "the keys", "the car"];
+  let when = ["before the class", "right on time", "when I finished"];
+
+  let whoIndex = Math.floor(Math.random() * who.length);
+  let actionIndex = Math.floor(Math.random() * action.length);
+  let whatIndex = Math.floor(Math.random() * what.length);
+  let whenIndex = Math.floor(Math.random() * when.length);
+
+  return (
+    who[whoIndex] +
+    " " +
+    action[actionIndex] +
+    " " +
+    what[whatIndex] +
+    " " +
+    when[whenIndex]
+  );
 };
